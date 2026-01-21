2706ef2b5b067a1059911ec18b5fda5813f8ef28
Hannah <grecco327@gmail.com>
2026-01-21 20:10:35 -0300

    props exercise

---
+++ src/components/Props/User.vue
@@ -0,0 +1,23 @@
+<script setup>
+import UserCard from './UserCard.vue';
+import { ref } from 'vue';
+
+    const users = ref([
+        { id: 1, name: 'Hannah', email: 'Hannah@gmail.com', isActive: false },
+        { id: 2, name: 'João', email: 'Joao@gmail.com', isActive: true },
+        { id: 3, name: 'Túlio', email: 'Tulio@gmail.com', isActive: true },
+])
+</script>
+<template>
+    <div>
+        <h2>User Card</h2>
+        <UserCard
+            v-for="user in users"
+            :key="user.id"
+            :name="user.name"
+            :email="user.email"
+            :isActive="user.isActive"
+        
+        />
+    </div>
+</template>
---
+++ src/components/Props/UserCard.vue
@@ -0,0 +1,14 @@
+<script setup>
+    const props = defineProps({
+        name: String,
+        email: String,
+        isActive: Boolean
+    })
+</script>
+
+<template>
+
+    <p>{{ name }}</p>
+    <p>{{ email }}</p>
+    <span>{{ isActive ? 'Ativo' : 'Inativo'  }}</span>
+</template>
